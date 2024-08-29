1284389465ab0dccee0f08e08b416836ab34aeb5
Kartikjain2626 <kartikjain2626@gmail.com>
2024-08-29 14:59:34 +0530

    Traning Task Day 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from "./Button" 
+function App(){
+  return (
+    <div>
+      <Button/>
+      <h1 style={{color:'red'}}>react</h1>
+      <input maxLength={5}/>
+
+     
+    </div>
+
+  )
+ }
+
+export default App
---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+function Button(){
+    return(  
+      <div>
+          <button>click here </button>
+      </div>
+      )
+  
+  }
+  export default Button;
+  
+  // index.html >>>>> main.jsx >>>>> app.jsx >>>>all component
