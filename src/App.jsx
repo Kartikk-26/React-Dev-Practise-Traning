e8153b8222bf93e39de500f495fb6656ea735144
Kartikjain2626 <kartikjain2626@gmail.com>
2024-08-29 21:44:57 +0530

    React Day 1 Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import Button from "./Button" 
-function App(){
-  const styles={color:'red',backgroundColor:'blue'};
+import Button from "./Button";
+import Footer from "./Footer";
+import NavBar from "./NavBar"; // Use NavBar instead of Navbar
+import './App.css'; // You might still need this for any custom styles
+
+function App() {
+  const styles = { color: 'red', backgroundColor: 'blue' };
+  
   return (
-    <div>
-      <Button text="click"/>
-      <Button text="Submit"/>
-      <Button text="apply"/>
-      <Button text="contact"/>
-      <h1 style={styles}>react</h1>
-      <input maxLength={5}/>
-
-     
+    <div className="flex flex-col min-h-screen">
+      <NavBar /> {/* Add the NavBar component here */}
+      <div className="flex-grow">
+        {/* Main content */}
+        <Button text="click" />
+        <Button text="Submit" />
+        <Button text="apply" />
+        <Button text="contact" />
+        <h1 style={styles}>React</h1>
+        <input maxLength={5} />
+      </div>
+      <Footer />
     </div>
+  );
+}
 
-  )
- }
-
-export default App
+export default App;
